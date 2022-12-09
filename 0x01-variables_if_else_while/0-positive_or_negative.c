6366e252729896df1d9e8809265d51a707ad7f5f
Victor Mugambi <gambikimathi@gmail.com>
2022-12-09 12:07:48 -0800

    positive is good

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,6 @@
 #include <stdlib.h>
-
 #include <time.h>
-
 #include <stdio.h>
-
 
 
 /**
@@ -14,21 +11,23 @@
 
 int main(void)
 {
-	int n;
+        int n;
 
 	srand(time(0))
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
 
-				printf("%d is positive\n", n);
+		printf("%d is positive\n", n);
 
-		else if (n == 0)
+	else if (n == 0)
 
-					printf("%d is zero\n", n);
+		printf("%d is zero\n", n);
 
-		else
+	else
 
-					printf("%d is negative\n", n);
-		return(0);
+		printf("%d is negative\n", n);
+
+        return(0);
 }
 
