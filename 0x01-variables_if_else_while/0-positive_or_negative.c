457de07ae00f097998d490edd496b2e7b3dbf332
Victor Mugambi <gambikimathi@gmail.com>
2022-12-10 05:45:49 -0800

    betty update on 0-positive file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,12 +11,12 @@
 
 int main(void)
 {
-        int n;
+	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n > 0)
+	if  (n > 0)
 
 		printf("%d is positive\n", n);
 
@@ -28,6 +28,6 @@ int main(void)
 
 		printf("%d is negative\n", n);
 
-        return(0);
+	return (0);
 }
 
