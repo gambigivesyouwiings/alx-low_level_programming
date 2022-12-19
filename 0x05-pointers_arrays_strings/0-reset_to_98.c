2aa9c05c5f6294193e63ee3573ed3af01fe95967
Victor Mugambi <gambikimathi@gmail.com>
2022-12-19 02:32:55 -0800

    betty checks

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,10 @@
 #include "main.h"
 
 /**
- * main - main block
- * 
- * Return: 0
- */
- void reset_to_98(int* n)
- {
-     *n = 98;
- }
-
+*reset_to_98  - is a void function reseting parameter
+*@n: is the variable
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
