29a1f12d3be5ae20b77eb786a8b04f8306ac5fc5
victor mugambi <gambikimathi@gmail.com>
2023-01-10 05:52:39 -0800

    lib update

--- 0x09-static_libraries/0-putchar.c
+++ 0x09-static_libraries/0-putchar.c
@@ -1,22 +1,2 @@
-#include "main.h"
-
-/**
-*main - prints the word Holberton
-*
-*Return: 0 on success execution.
-*/
-
-int main(void)
-{
-	char s[] = "_putchar";
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		_putchar(s[i]);
-		i++;
-	}
-	_putchar(10);
-
-	return (0);
-}
+int _putchar(char c)
+{printf;}
