0e8fdd9aa18427840b595df78f295ec7e0149530
victor mugambi <gambikimathi@gmail.com>
2023-01-17 05:16:49 -0800

    dog header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOGGIE_H
+#define DOGGIE_H
+
+/**
+* struct dog - contains dog information
+* @name:  name of dog
+* @age: age of the dog
+* @owner: owner of the dog
+* Description: Hods all needed data
+*/
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+}
+#endif /*DOGGIE*/
