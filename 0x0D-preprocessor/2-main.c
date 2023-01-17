030ef72eb933196555cbb13c798424a0365829fa
victor mugambi <gambikimathi@gmail.com>
2023-01-17 03:35:50 -0800

    this prints the origin file containing the program

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -4,9 +4,10 @@
 /**
 *main-This is a main file
 * prints the location of file
+* Return: 0 done
 */
 int main(void)
 {
-    printf("%s\n", __FILE__);
-    return 0;
+	printf("%s\n", __FILE__);
+	return (0);
 }
