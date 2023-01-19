676a74fc448c9c322f0d6eaacee2bf7298dcae46
victor mugambi <gambikimathi@gmail.com>
2023-01-19 05:19:01 -0800

    function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+
 /**
 * print_name - prints name of function
 *@name: text to be entered
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,12 @@
 #ifndef FUNC
 #define FUNC
 
-#include <studio.h>
+/*
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0E-function_pointers directory.
+ */
+
+#include <stdlib.h>
 
 int _putchar(char c);
 void print_name(char *name, void(*f)(char *));
