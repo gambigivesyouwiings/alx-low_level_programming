1c44adc3c599001c52a20fedaaf1d5e76a6fd311
victor mugambi <gambikimathi@gmail.com>
2023-01-19 06:02:12 -0800

    update

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,13 @@
+/**
+* array_iterator - void function name
+* @array: entered array
+* @size: number of array elements
+* @action: function pointer
+*/
+void array_iterator(int *array, size_t size, void(*action)(int))
+{
+	for (int i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
